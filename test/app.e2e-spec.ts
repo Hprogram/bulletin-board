13f449c2aa24c6f2249af5fda26803a9c0877c9e
Hprogram <wjs2096@gmail.com>
2022-11-07 03:12:03 +0900

    test case 작성 중간 저장

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,22 +5,43 @@ import { config } from 'dotenv';
 import { resolve } from 'path';
 import { AppModule } from '../src/app.module';
 import request from 'supertest';
+import { getConnection, Repository } from 'typeorm';
+import { User } from 'src/user/user.model';
+import { Board } from 'src/board/board.model';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
+  let userRepository: Repository<User>;
+  let boardRepository: Repository<Board>;
   beforeEach(async () => {
     config({ path: resolve(__dirname, `../.${process.env.NODE_ENV}.env`) });
     console.log(process.env.NODE_ENV);
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    userRepository = moduleFixture.get<Repository<User>>(
+      getRepositoryToken(User),
+    );
+    boardRepository = moduleFixture.get<Repository<Board>>(
+      getRepositoryToken(Board),
+    );
     await app.init();
   });
 
-  it('get hello', () => {
+  afterAll(async () => {
+    // e2e 테스트 후 db drop
+    await getConnection().dropDatabase();
+    app.close();
+  });
+
+  // 기본 테스트
+  it('get hello', async () => {
     return request(app.getHttpServer())
       .post('/graphql')
       .send({ query: '{hello}' })
@@ -30,17 +51,80 @@ describe('AppController (e2e)', () => {
       });
   });
 
-  it('user create', () => {
+  // 유저 생성 테스트
+  it('user create', async () => {
     const name = 'seoltab';
     return request(app.getHttpServer())
       .post('/graphql')
-      .send({ query: `mutation {createUser(name: "${name}"){name}}` })
+      .send({
+        query: `mutation {
+        createUser(param:{
+          name: "${name}"
+        }){
+          done
+          user{
+            id
+            name
+          }
+        }
+      }`,
+      })
       .expect(200)
       .expect(({ body }) => {
-        expect(body.data.createUser.name).toBe(name);
+        expect(body.data.createUser.user.name).toBe(name);
       });
   });
 
+  // 유저 검색 테스트
+  it('user search', () => {
+    const name = 'seoltab';
+    const id = 1;
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `query {
+        searchUser(param:{
+          id: ${id}
+        }){
+          done
+          user{
+            id
+            name
+          }
+        }
+      }`,
+      })
+      .expect(200)
+      .expect(({ body }) => {
+        expect(body.data.searchUser.user.name).toBe(name);
+      });
+  });
+
+  // // 유저 삭제 테스트
+  // it('user delete', () => {
+  //   const name = 'seoltab';
+  //   return request(app.getHttpServer())
+  //     .post('/graphql')
+  //     .send({
+  //       query: `mutation {
+  //       deleteUser(param:{
+  //         name: "${name}"
+  //       }){
+  //         done
+  //         user{
+  //           id
+  //           name
+  //         }
+  //       }
+  //     }`,
+  //     })
+  //     .expect(200)
+  //     .expect(({ body }) => {
+  //       expect(body.data.deleteUser.user.name).toBe(name);
+  //     });
+  // });
+
+  // 유저 콘텐츠 생성 테스트
   it('user create board', () => {
     const board = {
       title: '설탭 소개',
@@ -52,25 +136,55 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .post('/graphql')
       .send({
-        query: `mutation {createBoard(title: "${board.title}", content:"${board.content}", userName:"${name}"){content}}`,
+        query: `mutation {
+          createBoard(param:{
+            title: "${board.title}",
+            content:"${board.content}", 
+            userName:"${name}"
+          }){
+            done
+            board{
+              id
+              title
+              content
+              author{
+                id
+                name
+              }
+            }
+          }
+        }`,
       })
       .expect(200)
       .expect(({ body }) => {
-        expect(body.data.createBoard.title).toBe(board.title);
+        expect(body.data.createBoard.board.title).toBe(board.title);
+        expect(body.data.createBoard.board.content).toBe(board.content);
+        expect(body.data.createBoard.board.author.name).toBe(name);
       });
   });
+  // 유저가 쓴 글 모두 가져오기
+  // it('boards of user', () => {
+  //   const name = 'seoltab';
 
-  it('boards of user', () => {
-    const name = 'seoltab';
+  //   return request(app.getHttpServer())
+  //     .post('/graphql')
+  //     .send({
+  //       query: `query {
+  //         getBoards(param:{
+  //           userName:"${name}"
+  //         }){
+  //           board{
+  //             author {
+  //               name
+  //             }
+  //           }
 
-    return request(app.getHttpServer())
-      .post('/graphql')
-      .send({
-        query: `query {getBoards(userName:"${name}"){author {name}}}`,
-      })
-      .expect(200)
-      .expect(({ body }) => {
-        expect(body.data.getBoards).toBe(expect.arrayContaining(['author']));
-      });
-  });
+  //         }
+  //       }`,
+  //     })
+  //     .expect(200)
+  //     .expect(({ body }) => {
+  //       expect(body.data.getBoards).toBe(expect.arrayContaining(['author']));
+  //     });
+  // });
 });
